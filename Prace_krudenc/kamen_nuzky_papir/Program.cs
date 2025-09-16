16cc41822eec405523b5a57e3f1baab7a7ad7312
Jiří Krudenc <krudencj24@spstrutnov.cz>
2025-09-16 11:35:18 +0200

    Kámen Nůžky Papír

--- Prace_krudenc/kamen_nuzky_papir/Program.cs
+++ Prace_krudenc/kamen_nuzky_papir/Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] moznosti = { "kámen", "nůžky", "papír" };
+
+            Console.WriteLine("Vyber si kámen, nůžky nebo papír");
+            string uzivatel = Console.ReadLine().ToLower();
+            
+            Random program = new Random();
+
+            string pocitac = moznosti[program.Next(moznosti.Length)]; // náhodný výběr počítače
+
+            while (true)
+            {
+                while (uzivatel != "kámen" && uzivatel != "nůžky" && uzivatel != "papír")
+                {
+                    Console.WriteLine("Neplatná volba, zkus to znovu.");
+                    uzivatel = Console.ReadLine().ToLower();
+                }
+
+                Console.WriteLine($"Počítač si vybral: {pocitac}");
+
+                if (pocitac == "kámen" && uzivatel == "nůžky" ||
+                     pocitac == "nůžky" && uzivatel == "papír" ||
+                     pocitac == "papír" && uzivatel == "kámen")
+                {
+                    Console.WriteLine("Prohrál jsi!");
+                    
+                }
+                else if (pocitac == "kámen" && uzivatel == "papír" ||
+                          pocitac == "nůžky" && uzivatel == "kámen" ||
+                          pocitac == "papír" && uzivatel == "nůžky")
+                {
+                    Console.WriteLine("Vyhrál jsi!");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Remíza!");
+                    
+                }
+                Console.WriteLine("Chceš hrát znovu? (ano/ne)");
+                string znovu = Console.ReadLine().ToLower();
+                if (znovu == "ano")
+                {
+                    Console.WriteLine("Vyber si kámen, nůžky nebo papír");
+                    uzivatel = Console.ReadLine().ToLower();
+                    pocitac = moznosti[program.Next(moznosti.Length)];
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
         }
     }
 }
